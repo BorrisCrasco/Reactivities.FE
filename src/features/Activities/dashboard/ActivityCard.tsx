a95157debf8eea34570318f3065d35d25d787e3a
philip crasco <philiplorenzcrasco@gmail.com>
2025-04-10 14:53:50 +0800

    End Of Section 6

--- src/features/Activities/dashboard/ActivityCard.tsx
+++ src/features/Activities/dashboard/ActivityCard.tsx
@@ -1,13 +1,16 @@
 import { Box, Button, Card, CardActions, CardContent, Chip, Typography } from "@mui/material"
+import { useActivities } from "../../../lib/hooks/useActivities"
 
 type Props = {
     activity: Activity
     selectActivity: (id: string) => void
-    deleteActivity : (id : string) => void
 }
 
 
-export default function ActivityCard({ activity, selectActivity,deleteActivity }: Props) {
+export default function ActivityCard({ activity, selectActivity }: Props) {
+
+    const {deleteActivity} = useActivities();
+
     return (
         <Card sx={{ borderRadius: 3 }}>
             <CardContent>
@@ -28,7 +31,8 @@ export default function ActivityCard({ activity, selectActivity,deleteActivity }
                     </Button>
 
                     <Button onClick={() =>
-                        deleteActivity(activity.id)}
+                        deleteActivity.mutate(activity.id)}
+                        disabled={deleteActivity.isPending}
                         size="medium"
                         color="error"
                         variant="contained">
--- src/features/Activities/details/ActivityDetail.tsx
+++ src/features/Activities/details/ActivityDetail.tsx
@@ -1,18 +1,25 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
+import { useActivities } from "../../../lib/hooks/useActivities"
 
 
 type Props = {
-    activity: Activity
+    selectedActivity: Activity
     cancelSelectActivity: () => void
     openForm: (id: string) => void
 
 }
 
 export default function ActivityDetail({
-    activity,
+    selectedActivity ,
     cancelSelectActivity,
     openForm }
     : Props) {
+
+        const {activities} = useActivities();
+        const activity = activities?.find(x => x.id === selectedActivity.id);
+
+        if(!activity) return <Typography>Loading...</Typography>
+
     return (
         <Card sx={{ borderRadius: 3 }}>
             <CardMedia component='img'
