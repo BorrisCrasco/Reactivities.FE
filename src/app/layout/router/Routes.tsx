adf0e8716de07b8a6a2781daf776ae961409881b
philip crasco <philiplorenzcrasco@gmail.com>
2025-04-12 13:31:50 +0800

    End Of Section 8

--- src/app/layout/router/Routes.tsx
+++ src/app/layout/router/Routes.tsx
@@ -3,7 +3,7 @@ import App from "../App";
 import HomePage from "../../../features/home/HomePage";
 import ActivityDashboard from "../../../features/Activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../../features/Activities/Form/ActivityForm";
-import ActivityDetail from "../../../features/Activities/details/ActivityDetail";
+import ActivityDetailPage from "../../../features/Activities/details/ActivityDetailPage";
 
 
 export const router = createBrowserRouter([
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
         children:[
             {path: '',element: <HomePage/>},
             {path: 'activities',element: <ActivityDashboard/>},
-            {path: 'activities/:id',element: <ActivityDetail/>},
+            {path: 'activities/:id',element: <ActivityDetailPage/>},
             {path: 'createActivity', element:<ActivityForm key='create'/>},
             {path: 'manage/:id', element:<ActivityForm/>}
 
